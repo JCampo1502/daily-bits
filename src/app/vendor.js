a65f81f9e84c80137f3457f73df71a54bd4b1fb4
Javier D. Campo <javierdavidcamposuarez15@gmail.com>
2024-07-29 20:37:45 -0500

    build: :tada: Let´s start with the basic structure

---
+++ src/app/vendor.js
@@ -0,0 +1,5 @@
+// Import our custom CSS
+import '../scss/styles.scss'
+import img from './../assets/caracters-1.png';
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
---
+++ webpack.config.js
@@ -0,0 +1,102 @@
+'use strict'
+const path = require('path')
+const autoprefixer = require('autoprefixer')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode:'development',
+    entry:{
+        app:'./src/app/main.js',
+        vendor: './src/app/vendor.js'
+    },
+    output:{
+        filename:'./script/[name].[contenthash].js',
+        path:path.resolve(__dirname,'dist'),
+        assetModuleFilename: 'assets/[hash][ext][query]',
+        clean:true
+    },
+    devServer: {
+        static: path.resolve(__dirname,'dist'),
+        port:8080,
+        hot:true
+    },
+    plugins:[
+        new HtmlWebpackPlugin({template:'./src/index.html'}),
+        new HtmlWebpackPlugin({
+            template:'./src/pages/home.html',
+            filename:'./pages/home.html'
+        }),
+        new HtmlWebpackPlugin({
+            template:'./src/pages/game.html',
+            filename:'./pages/game.html'
+        }),
+        new HtmlWebpackPlugin({
+            template:'./src/pages/account.html',
+            filename:'./pages/account.html'
+        }),
+        new MiniCssExtractPlugin({filename: './styles/[name].[contenthash].css'}),
+    ],
+    module:{
+        rules: [           
+            {
+              mimetype: 'image/svg+xml',
+              scheme: 'data',
+              type: 'asset/resource',
+              generator: {
+                filename: 'assets/icons/[hash].svg'
+              }
+            },
+            {
+                test:/\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset',
+                generator: {
+                    filename: 'assets/images/[name].[ext]'
+                }
+            },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ],
+                        plugins: [
+                            '@babel/plugin-proposal-optional-chaining',
+                            '@babel/plugin-transform-runtime',
+                        ]
+                    }
+                }
+            },
+            {
+                test:/.(scss)$/,
+                use:[
+                    {
+                        loader:MiniCssExtractPlugin.loader
+                    },
+                    {
+                        // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                        loader: 'css-loader'
+                    },
+                    {
+                        // Loader for webpack to process CSS with PostCSS
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                            plugins: [
+                                autoprefixer
+                            ]
+                        }
+                    }
+                    },
+                    {
+                        // Loads a SASS/SCSS file and compiles it to CSS
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    }
+}
